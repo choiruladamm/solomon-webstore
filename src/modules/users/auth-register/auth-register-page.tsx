2960d6b861b288cea369381848e3c6651f2455cd
choiruladamm <choiruladamm@gmail.com>
2024-05-29 10:51:18 +0700

    feat(auth): wip layout auth user

---
+++ src/modules/users/auth-register/auth-register-page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface AuthRegisterPageProps {}
+
+const AuthRegisterPage: FC<AuthRegisterPageProps> = ({}) => {
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <div className="layout"></div>
+    </main>
+  );
+};
+
+export default AuthRegisterPage;
---
+++ src/modules/users/auth-register/form-auth-register.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { FC } from 'react';
+
+interface FormAuthRegisterProps {}
+
+const FormAuthRegister: FC<FormAuthRegisterProps> = ({}) => {
+  return <div>FormAuthRegister</div>;
+};
+
+export default FormAuthRegister;
